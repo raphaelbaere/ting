d385d2119f57af3daa49229589c2745ac55d0168
raphaelbaere <raphaelbaere@id.uff.br>
2023-06-14 18:13:56 -0300

    Requisito 7 concluído

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,28 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    r = []
+    c = 0
+
+    for i in range(len(instance)):
+        q = instance.search(i)
+        ins_obj = {
+            "palavra": word,
+            "arquivo": q["nome_do_arquivo"],
+            "ocorrencias": [
+                {"linha": index + 1}
+                for index, line in enumerate(q["linhas_do_arquivo"])
+                if word.lower() in line.lower()
+            ],
+        }
+
+        r.append(ins_obj)
+
+        if not len(ins_obj["ocorrencias"]):
+            c += 1
+
+    if c == len(instance):
+        return []
+
+    return r
 
 
 def search_by_word(word, instance):
