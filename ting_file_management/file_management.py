d732565eb4d196abdf0520a1126b8599857c9a9f
raphaelbaere <raphaelbaere@id.uff.br>
2023-06-14 14:55:58 -0300

    Requisito 2 concluído com sucesso

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,12 @@
+import sys
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    if 'txt' not in path_file:
+        sys.stderr.write("Formato inválido")
+        return "Formato inválido"
+    try:
+        with open(path_file, "r") as file:
+            content = file.read()
+        return content.split('\n')
+    except FileNotFoundError:
+        sys.stderr.write(f"Arquivo {path_file} não encontrado\n")
